0302e8d6dfbb41d41a02f53eb221b6fab32874cc
Nguyen Duong <muoi07052001@gmail.com>
2022-06-10 17:00:06 +0700

    Add comment day 3 semantics

--- bai4/day3/completed/semantics.c
+++ bai4/day3/completed/semantics.c
@@ -12,6 +12,8 @@
 extern SymTab *symtab;
 extern Token *currentToken;
 
+// Trả về thông tin của 1 Object
+// Tìm trong tất cả Scope của chương trình
 Object *lookupObject(char *name)
 {
   // TODO
@@ -20,29 +22,39 @@ Object *lookupObject(char *name)
 
   while (currentScope != NULL)
   {
+    // Tìm Object trong list các Objects của Scope hiện tại
     object = findObject(currentScope->objList, name);
     if (object != NULL)
     {
       return object;
     }
+    // Nếu không tìm thấy -> Tìm Object ở Scope bên ngoài
     currentScope = currentScope->outer;
   }
 
+  // Tìm Object ở Scope Global (Scope ngoài cùng, ngoài PROGRAM)
   object = findObject(symtab->globalObjectList, name);
   if (object != NULL)
     return object;
 
   return NULL;
 }
+// Kiểm tra xem IDENT này đã được định nghĩa TRONG SCOPE HIỆN TẠI chưa?
+// Dùng khi khai báo
 void checkFreshIdent(char *name)
 {
   // TODO
+  // Nếu tìm thấy IDENT đã được định nghĩa -> Báo lỗi
   if (findObject(symtab->currentScope->objList, name) != NULL)
   {
     error(ERR_DUPLICATE_IDENT, currentToken->lineNo, currentToken->colNo);
   }
 }
 
+// Check xem IDENT đã được khai báo chưa (tìm trong cả Scope ngoài)
+// Nếu đã khai báo -> Trả về Object có tên là IDENT đấy
+// Nếu chưa khai báo -> Trả về lỗi
+// Dùng khi gán
 Object *checkDeclaredIdent(char *name)
 {
   // TODO
@@ -55,6 +67,10 @@ Object *checkDeclaredIdent(char *name)
   return object;
 }
 
+// Check xem CONSTANT đã được khai báo chưa (tìm trong cả Scope ngoài)
+// Nếu đã khai báo -> Trả về Object có tên là tên của CONSTANT đấy
+// Nếu chưa khai báo -> Trả về lỗi
+// Dùng khi gán
 Object *checkDeclaredConstant(char *name)
 {
   // TODO
@@ -69,6 +85,10 @@ Object *checkDeclaredConstant(char *name)
   return obj;
 }
 
+// Check xem TYPE đã được khai báo chưa (tìm trong cả Scope ngoài)
+// Nếu đã khai báo -> Trả về Object có tên là tên của TYPE đấy
+// Nếu chưa khai báo -> Trả về lỗi
+// Dùng khi gán
 Object *checkDeclaredType(char *name)
 {
   // TODO
@@ -81,6 +101,10 @@ Object *checkDeclaredType(char *name)
   return obj;
 }
 
+// Check xem VAR đã được khai báo chưa (tìm trong cả Scope ngoài)
+// Nếu đã khai báo -> Trả về Object có tên là tên của VAR đấy
+// Nếu chưa khai báo -> Trả về lỗi
+// Dùng khi gán
 Object *checkDeclaredVariable(char *name)
 {
   // TODO
@@ -93,24 +117,34 @@ Object *checkDeclaredVariable(char *name)
   return obj;
 }
 
+// Check xem FUNCTION đã được khai báo chưa (tìm trong cả Scope ngoài)
+// Nếu đã khai báo -> Trả về Object có tên là tên của FUNCTION đấy
+// Nếu chưa khai báo -> Trả về lỗi
+// Dùng khi gán
 Object *checkDeclaredFunction(char *name)
 {
   // TODO
   Object *obj = lookupObject(name);
   if (obj == NULL)
     error(ERR_UNDECLARED_FUNCTION, currentToken->lineNo, currentToken->colNo);
+  // Nếu tìm đc Object nhưng kiểu ko p FUNCTION -> báo lỗi
   if (obj->kind != OBJ_FUNCTION)
     error(ERR_INVALID_FUNCTION, currentToken->lineNo, currentToken->colNo);
 
   return obj;
 }
 
+// Check xem PROCEDURE đã được khai báo chưa (tìm trong cả Scope ngoài)
+// Nếu đã khai báo -> Trả về Object có tên là tên của PROCEDURE đấy
+// Nếu chưa khai báo -> Trả về lỗi
+// Dùng khi gán
 Object *checkDeclaredProcedure(char *name)
 {
   // TODO
   Object *obj = lookupObject(name);
   if (obj == NULL)
     error(ERR_UNDECLARED_PROCEDURE, currentToken->lineNo, currentToken->colNo);
+  // Nếu tìm đc Object nhưng kiểu ko p PROCEDURE -> báo lỗi
   if (obj->kind != OBJ_PROCEDURE)
     error(ERR_INVALID_PROCEDURE, currentToken->lineNo, currentToken->colNo);
 
